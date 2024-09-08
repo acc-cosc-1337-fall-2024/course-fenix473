ffc394238bac93fe330a925f3dc4227f1f3a5c4b
fenix473 <131599429+fenix473@users.noreply.github.com>
2024-09-08 03:22:18 +0000

    Now it works, some comments added to main.cpp other minor readablitiy fixes in cout part.

--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -5,18 +5,25 @@
 
 //write namespace using statement for cout
 
+
+// Open main function
 int main()
 {
+	//Declare num variable.
 	int num;
-	std::cout << "Enter a number please" << std::endl;
+	//Cout to communicate request and cin to save it to num variable.
+	std::cout << "Enter a number please: ";
 	std::cin >> num;
 
+	//make result_free variable to perform and store multiply numbers operation.
 	int result_free = multiply_numbers(num);
-	std::cout << "Result number is: " << result_free << std::endl;
+	//Using cout show text and result
+	std::cout << "Result user number multiplied by 5 is: " << result_free << std::endl;
 
+	//Hardcode num1 variable as 4 and 
 	int num1 = 4;
 	int result_4 = multiply_numbers(num1);
-	std::cout << "Result number with factor of 4 is: " << result_4 << std::endl;
+	std::cout << "Result number after fixing num1 variable as 4 is: " << result_4 << std::endl;
 
 	return 0;
 }
