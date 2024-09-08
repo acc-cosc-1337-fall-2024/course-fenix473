28346943071d8a969386c26e8c0e389e98f38d80
fenix473 <131599429+fenix473@users.noreply.github.com>
2024-09-08 03:34:02 +0000

    Commented header and data_types files.

--- src/homework/01_data_types/data_types.cpp
+++ src/homework/01_data_types/data_types.cpp
@@ -8,8 +8,11 @@ int add_numbers(int num1, int num2)
 
 //write function code here
 
+// Function mechanics code, with num1 passed as an argument multiply it by num2.
 int multiply_numbers(int num1)
 {
+	// Num2 is hardcoded as 5.
 	int num2 = 5;
+	// Return result of multiplication to caller.
 	return num1 * num2;
 }
--- src/homework/01_data_types/data_types.h
+++ src/homework/01_data_types/data_types.h
@@ -2,4 +2,5 @@
 int add_numbers(int num1, int num2);
 //write function prototype here
 
+// Function declaration, name and parametrs it can handle.
 int multiply_numbers(int num1);
