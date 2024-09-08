df8059b122d8ebc68b3c98ddc75f42a37ccddff6
fenix473 <131599429+fenix473@users.noreply.github.com>
2024-09-08 03:05:09 +0000

    test1 9/7 10:05pm

--- src/homework/01_data_types/data_types.cpp
+++ src/homework/01_data_types/data_types.cpp
@@ -7,7 +7,3 @@ int add_numbers(int num1, int num2)
 }
 
 //write function code here
-
-
-
-
--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -1,12 +1,22 @@
 ﻿//write include statements
+#include "data_types.h"
+#include <iostream>
 
 
 //write namespace using statement for cout
 
-
 int main()
 {
-	
+	int num;
+	std::cout << "Enter a number please" << std::endl;
+	std::cin >> num;
+
+	int result_free = multiply_numbers(num);
+	std::cout << "Result number is: " << result_free << std::endl;
+
+	int num1 = 4;
+	int result_4 = multiply_numbers(num1);
+	std::cout << "Result number with factor of 4 is: " << result_4 << std::endl;
 
 	return 0;
 }
