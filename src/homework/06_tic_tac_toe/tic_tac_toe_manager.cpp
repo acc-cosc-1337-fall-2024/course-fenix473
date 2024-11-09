743496a920911b6274cf3850fc0bfd11f524517d
fenix473 <131599429+fenix473@users.noreply.github.com>
2024-11-09 18:52:29 +0000

    .h defined, need to write cpp functionality. No straightforward way to get winner.

--- src/homework/06_tic_tac_toe/tic_tac_toe_manager.cpp
+++ src/homework/06_tic_tac_toe/tic_tac_toe_manager.cpp
@@ -1,1 +1,9 @@
 //cpp
+
+#include "tic_tac_toe_manager.h"
+
+void TicTacToeManager::save_game(TicTacToe game){
+    games.push_back(game);
+    update_winner_count(game.get_winner);
+    // We don't have clear winner determiner lol
+}
--- src/homework/06_tic_tac_toe/tic_tac_toe_manager.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe_manager.h
@@ -1,1 +1,20 @@
 //h
+
+#define tic_tac_toe_manager_H
+
+#include "tic_tac_toe.h"
+#include <vector>
+#include <string>
+
+class TicTacToeManager {
+private:
+    std::vector<TicTacToe> games;
+    int x_win{0};
+    int o_wins{0};
+    int ties{0};
+    void update_winner_count(std::string vinner);
+
+public:
+    void save_game(TicTacToe game);
+    void get_winner_totals(int& ,int& ,int&);
+};
